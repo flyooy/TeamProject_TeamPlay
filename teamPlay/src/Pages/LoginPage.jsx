7cdd1696b309dbb2473f05fed0df404285e25081
René Behrens <rene.behrens@aol.com>
2024-10-13 17:18:19 +0200

    del userData fetch on LoginPage

--- teamPlay/src/Pages/LoginPage.jsx
+++ teamPlay/src/Pages/LoginPage.jsx
@@ -47,70 +47,13 @@ export default function LoginPage() {
         console.log("Auth response data:", data); 
         
         localStorage.setItem("token", data.token);
-        sessionStorage.setItem("token", data.token); 
-        
-        
-       
-        return fetchUserDashboard(data.token);
-      })
-      .then((userData) => {
-        console.log("User dashboard data:", userData); 
-
-        if (userData.id) {
-          console.log("User ID found:", userData.id);
-          localStorage.setItem("userId", userData.id); // Сохраняем userId из userData
-      } else {
-          console.error("No userId found in userData");
-      }
-        if (userData.name) {
-          console.log("User name found:", userData.name);
-          localStorage.setItem("userName", userData.name); 
-          setUserName(userData.name); 
-        } else {
-          console.error("No user name found in response");
-        }
-        if (userData.winRatio !== undefined) {
-          console.log("User win ratio found:", userData.winRatio);
-          localStorage.setItem("winRatio", userData.winRatio); 
-          setWinRatio(userData.winRatio); 
-        } else {
-          console.error("No win ratio found in response");
-        }
-        if (userData.teamDTO) {
-          const teamInfo = {
-            name: userData.teamDTO.teamName, // Получаем teamName
-            members: userData.teamDTO.Players.map(player => player.name || "Unnamed")
-          };
-          localStorage.setItem("teamInfo", JSON.stringify(teamInfo));
-          setTeamInfo(teamInfo);
-      } else {
-          console.error("No team data found in response");
-          localStorage.removeItem("teamInfo");
-        setTeamInfo({ name: '', members: [] });
-      }
-
         navigate("/user");
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
 
-  const fetchUserDashboard = (token) => {
-    return fetch("http://localhost:8080/api/v1/dash", {
-      method: "GET",
-      headers: {
-        "Authorization": "Bearer " + token,
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Failed to fetch user dashboard');
-        }
-        return response.json();
-      });
-  };
 
   return (
     <main>
